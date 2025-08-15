aceba5f760b2cb9657ddbe653a97659fbaa9124e
Piyush Kumar <24145@iiitu.ac.in>
2025-08-15 19:46:06 +0530

    app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'; 
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
