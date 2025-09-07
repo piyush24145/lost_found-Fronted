faa8cace22fdd0d6296ebc0846b9c1ee1b662e0e
piyush kumar <24145@iiitu.ac.in>
2025-09-07 19:55:54 +0530

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,7 @@ let baseUrl = "";
 
 if (process.env.NODE_ENV === "production") {
   // ✅ Production (Vercel build ke liye)
-  baseUrl = "https://lost-found-backend-5.onrender.com";
+  baseUrl = "https://lost-found-backend-ur4j.onrender.com";
 } else {
   // ✅ Development (local chalane ke liye)
   baseUrl = "http://localhost:5000";
